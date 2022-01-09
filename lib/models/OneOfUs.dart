cbafdf16a28289f277b0457d6a1170c51ad6c1ae
paradoxon <blablabla!1865Atlassian>
2022-01-09 19:07:11 +0100

    prepared smoe more classes * KamonuMarker - a Marker to display a Person in Need (OneOfUs) * OneOfUs -  The Person to report with all necessary Informations * ReportOneOfUs - The Report which includes the Person (OneOfUs, Time and some more Informations like a way to report back the organisation which took care about this person)

---
+++ lib/models/OneOfUs.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+
+class Condition{
+  String name;
+  IconData icon;
+  String description="";
+  bool inCondition = true;
+  Condition(this.name,this.icon);
+}
+
+/*ToDo: need to move to the new List of possible gender to make shure that
+organisations can help according to the gender
+*/
+enum Gender {
+  notSpecified,
+  female,
+  male,
+  diverse,
+}
+
+class OneOfUs {
+  String title = "Obdachlose Person";
+  String notes = "";
+  String languages = "de";
+  bool helped = false;
+  Gender gender = Gender.notSpecified;
+  List<Condition> conditions = <Condition>[];
+  //ToDo: add Organisations wich helped
+
+  addCondition(Condition _condition){
+    conditions.add(_condition);
+  }
+
+}
