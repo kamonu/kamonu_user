122deb96743df869020515d319fa102ceec6c0f9
paradoxon <blablabla!1865Atlassian>
2022-02-17 22:02:42 +0100

    small cleanup and the button will now call report Homeless Person...

--- lib/main.dart
+++ lib/main.dart
@@ -32,16 +32,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -60,12 +50,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
@@ -73,16 +58,16 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+        // Center the kamonu Map
         child:_kamonuMap
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _kamonuMap.reportHomelessPerson,
+        onPressed: (){
+          _kamonuMap.reportHomelessPerson();
+        },
         tooltip: 'Increment',
         child: const Icon(Icons.add_location),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      )
     );
   }
 }
